0ec7c2fa2a9ba2348c2f846c3093ff207aaedc75
madhavareddy97056 <81456216+madhavareddy97056@users.noreply.github.com>
2024-10-01 10:53:23 +0530

    Update 16_jenkins_installation_ubuntu.sh added sleep

--- 16_jenkins_installation_ubuntu.sh
+++ 16_jenkins_installation_ubuntu.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 echo "================================================"
+echo "Updating OS packages"
+echo "================================================"
+sudo apt update -y > /dev/null
+sleep 5
+echo
+
+echo "================================================"
 echo "Installing jenkins dependency java 17"
 echo "================================================"
 sudo apt install openjdk-17-jdk -y > /dev/null
+sleep 5
 echo
 
 echo "================================================"
 echo "Verify java version"
 echo "================================================"
 java -version
+sleep 2
 echo
 
 echo "================================================"
@@ -19,24 +28,28 @@ sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debi
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
+    sleep 3
 echo
 
 echo "================================================"
 echo "Updating packages"
 echo "================================================"
 sudo apt update &> /dev/null
+sleep 5
 echo
 
 echo "================================================"
 echo "installing Jenkins"
 echo "================================================"
 sudo apt install jenkins -y > /dev/null
+sleep 5
 echo
 
 echo "================================================"
 echo "status of jenkins service"
 echo "================================================"
 sudo systemctl is-active jenkins
+sleep 1
 echo
 
 echo "================================================"
