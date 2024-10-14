7d9de5a4e11677c8df7e8ce758f86a0eff6f3fcf
madhavareddy97056 <81456216+madhavareddy97056@users.noreply.github.com>
2024-10-14 11:58:01 +0530

    Create docker_setup_ubuntu.sh file

---
+++ docker_setup_ubuntu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "Add Docker's official GPG key"
+echo "Updating apt repo"
+echo "==================================================="
+sudo apt-get update &> /dev/null
+sleep 20
+
+sudo apt-get install ca-certificates curl
+sleep 2
+sudo install -m 0755 -d /etc/apt/keyrings
+sleep 3
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sleep 3
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+sleep 2
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sleep 2
+echo "updating apt repo"
+echo "==================================================="
+sudo apt-get update
+sleep 3
+echo
+echo "installing ocker packages"
+echo "==================================================="
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y &> /dev/null
+echo
+
+echo "Docker status"
+echo "==================================================="
+sudo systemctl is-active docker
+echo
