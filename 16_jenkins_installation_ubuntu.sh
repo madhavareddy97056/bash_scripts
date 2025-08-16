c5a87cf0bb57ada07410ae1db8621cab880d7b10
madhavareddy97056 <81456216+madhavareddy97056@users.noreply.github.com>
2025-08-16 13:22:27 +0530

    Update 16_jenkins_installation_ubuntu.sh

--- 16_jenkins_installation_ubuntu.sh
+++ 16_jenkins_installation_ubuntu.sh
@@ -28,7 +28,7 @@ sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
-    sleep 3
+sleep 3
 echo
 
 echo "================================================"
